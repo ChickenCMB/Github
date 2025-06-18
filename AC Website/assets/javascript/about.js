f3111e439d9ba038db5cfdc30e562c199a237394
ChickenCMB <145242044+ChickenCMB@users.noreply.github.com>
2025-06-18 09:12:46 +0700

    Delete AC Website/assets/javascript directory

--- AC Website/assets/javascript/about.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("currentYear").textContent = new Date().getFullYear();
-
-function toggleSidebar() {
-  document.getElementById("sidebar").classList.toggle("open");
-}
-
-function toggleAboutModal() {
-  document.getElementById("aboutModal").classList.toggle("show");
-}
